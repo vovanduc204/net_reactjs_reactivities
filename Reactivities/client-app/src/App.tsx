88c43685a0ac2aec44ff2ead0b56997986160f74
vovanduc <voduc204@gmail.com>
2022-04-04 14:55:04 +0700

    setup mui

--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -1,35 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'semantic-ui-css/semantic.min.css';
-import { Button, Icon, Label } from 'semantic-ui-react';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <Button as='div' labelPosition='right'>
-          <Button color='red'>
-            <Icon name='heart' />
-            Like
-          </Button>
-          <Label as='a' basic color='red' pointing='left'>
-            2,048
-          </Label>
-        </Button>
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="female"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel value="female" control={<Radio />} label="Female" />
+            <FormControlLabel value="male" control={<Radio />} label="Male" />
+            <FormControlLabel value="other" control={<Radio />} label="Other" />
+          </RadioGroup>
+        </FormControl>
       </header>
     </div>
   );
